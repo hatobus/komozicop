58ef08e678f03ef59ce7dc2936457da395027c98
hatobus <s1240056@gmail.com>
2020-04-25 01:43:22 +0900

    TestKomoziCop test pass

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/hatobus/komizicop
+
+go 1.14
+
+require (
+	github.com/ikawaha/kagome v1.11.2
+	github.com/stretchr/testify v1.5.1
+)
---
+++ komozi_cop_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKomoziCop(t *testing.T) {
+	testcase := []struct {
+		testname       string
+		userpoststring string
+		usekomozi      bool
+	}{
+		{
+			testname:       "普通ののあの言葉を怒らないテスト",
+			userpoststring: "noah_orbergを冠してReoYamadaを名乗るとはええ度胸やな",
+			usekomozi:      false,
+		},
+		{
+			testname:       "語尾に小文字をつける時に怒る",
+			userpoststring: "のあだょ",
+			usekomozi:      true,
+		},
+	}
+
+	for _, tc := range testcase {
+		t.Run(tc.testname, func(t *testing.T) {
+			iskomoziuse, err := CheckIlligalKomoziUsing(tc.userpoststring)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tc.usekomozi, iskomoziuse)
+		})
+	}
+}
